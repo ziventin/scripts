78235ef7b58ce800f88d5e8221b8bc2ae65c83f3
ziventin <ziventin@gmail.com>
2017-07-23 15:05:41 +0300

    1.2

--- setup.sh
+++ setup.sh
@@ -1,51 +1,56 @@
 #!/bin/bash
 #an easy method to install my configs and scripts
 #for archlinux
+echo "starting scripts"
 
 # install git
-sudo pacman -S git
+echo "installing git"
+(sudo pacman -S git &)
+echo "DONE"
 
 #clone scripts and configs
+echo "cloning scripts and configs"
 git clone https://www.github.com/ziventin/scripts
+echo "DONE"
 cd scripts
 
 #put bashrc into the correct location
-ln -S bashrc ~/.bashrc
+echo "moving .bashrc"
+ln -s bashrc ~/.bashrc
+echo "DONE"
 
 #put xinitrc into the correct location
-ln -S xinitrc ~/.xinitrc
+echo "moving .xinitrc"
+ln -s xinitrc ~/.xinitrc
+echo "DONE"
 
 #put Xresources into the correct location
-ln -S Xresources ~/.Xresources
+ln -s Xresources ~/.Xresources
 
 #put i3 config into the correct location
 mkdir ~/.config
 mkdir ~/.config/i3
-ln -S i3_config ~/.config/i3/config
+ln -s i3_config ~/.config/i3/config
 
 #put vimrc into the correct location
-ln -S vimrc ~/.vimrc
+ln -s vimrc ~/.vimrc
 
 #clone my firefox start page
 git clone https://www.github/ziventin/startpage
 
-#enable multilib repo for steam
-echo "[multilib]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
-sudo pacman -Syu
-
 #install programs
-sudo pacman -S firefox rxvt-unicode feh mesa xorg-server lightdm lightdm-gtk-greeter screenfetch steam ttf_liberation 
+sudo pacman -S firefox rxvt-unicode feh mesa xorg lightdm lightdm-gtk-greeter screenfetch base_devel
 
-#enable lightdm
-systemctl enable lighdm.service
+#install cdm through AUR
+git clone https://aur.archlinux.org/cdm-git.git cdm
+cd cdm
+makepkg -si
+cd ..
+rm -rf cdm
 
-#install i3-gaps through the AUR
-git clone https://aut.archlinux.org/i3-gaps-git.git
+#install i3-gaps through AUR
+git clone https://aur.archlinux.org/i3-gaps-git.git
 cd i3-gaps-git
 makepkg -si
 cd ..
 rm -rf i3-gaps-git
-
-#reboot the system to apply effects
-reboot
