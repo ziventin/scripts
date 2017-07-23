5763dd3bbb38eb423bdc211412702defeed55c70
ziventin <ziventin@gmail.com>
2017-07-23 16:19:05 +0300

    ver 1.2

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #an easy method to install my configs and scripts
 #for archlinux
-echo "starting scripts"
+echo "starting script"
 
 # install git
 echo "installing git"
-(sudo pacman -S git &)
+sudo pacman -S git
 echo "DONE"
 
 #clone scripts and configs
@@ -25,32 +25,54 @@ ln -s xinitrc ~/.xinitrc
 echo "DONE"
 
 #put Xresources into the correct location
+echo "moving .Xresources"
 ln -s Xresources ~/.Xresources
+echo "DONE"
 
 #put i3 config into the correct location
 mkdir ~/.config
 mkdir ~/.config/i3
+echo "moving i3 config"
 ln -s i3_config ~/.config/i3/config
+echo "DONE"
 
 #put vimrc into the correct location
+echo "moving .vimrc"
 ln -s vimrc ~/.vimrc
+echo "DONE"
 
 #clone my firefox start page
+echo "cloning firefox startpage"
 git clone https://www.github/ziventin/startpage
+echo "DONE"
 
 #install programs
-sudo pacman -S firefox rxvt-unicode feh mesa xorg lightdm lightdm-gtk-greeter screenfetch base_devel
+echo "installing programs"
+sudo pacman -S firefox rxvt-unicode feh mesa xorg screenfetch base_devel vim
+echo "DONE"
 
 #install cdm through AUR
+echo "installing cdm"
+echo "cloning"
 git clone https://aur.archlinux.org/cdm-git.git cdm
 cd cdm
+echo "instaling"
 makepkg -si
 cd ..
 rm -rf cdm
+echo "DONE"
 
 #install i3-gaps through AUR
+echo "installing i3-gaps"
+echo "cloning"
 git clone https://aur.archlinux.org/i3-gaps-git.git
 cd i3-gaps-git
+echo "instaling"
 makepkg -si
 cd ..
 rm -rf i3-gaps-git
+echo "done"
+
+echo ""
+echo "all programs and scripts installed"
+exit
