25e38e72f64278881d30676c29fec26419264363
ziventin <ziventin@gmail.com>
2017-07-24 20:28:03 +0300

    ver 1.3

--- setup.sh
+++ setup.sh
@@ -16,34 +16,34 @@ cd scripts
 
 #put bashrc into the correct location
 echo "moving .bashrc"
-ln -s bashrc ~/.bashrc
+ln -sf ~/scripts/bashrc ~/.bashrc
 echo "DONE"
 
 #put xinitrc into the correct location
 echo "moving .xinitrc"
-ln -s xinitrc ~/.xinitrc
+ln -sf ~/scripts/xinitrc ~/.xinitrc
 echo "DONE"
 
 #put Xresources into the correct location
 echo "moving .Xresources"
-ln -s Xresources ~/.Xresources
+ln -sf ~/scripts/Xresources ~/.Xresources
 echo "DONE"
 
 #put i3 config into the correct location
 mkdir ~/.config
 mkdir ~/.config/i3
 echo "moving i3 config"
-ln -s i3_config ~/.config/i3/config
+ln -sf ~/scripts/i3_config ~/.config/i3/config
 echo "DONE"
 
 #put vimrc into the correct location
 echo "moving .vimrc"
-ln -s vimrc ~/.vimrc
+ln -sf ~/scripts/vimrc ~/.vimrc
 echo "DONE"
 
 #clone my firefox start page
 echo "cloning firefox startpage"
-git clone https://www.github/ziventin/startpage
+git clone https://www.github.com/ziventin/startpage
 echo "DONE"
 
 #install programs
@@ -51,26 +51,14 @@ echo "installing programs"
 sudo pacman -S firefox rxvt-unicode feh mesa xorg screenfetch base_devel vim
 echo "DONE"
 
-#install cdm through AUR
-echo "installing cdm"
-echo "cloning"
-git clone https://aur.archlinux.org/cdm-git.git cdm
-cd cdm
-echo "instaling"
-makepkg -si
-cd ..
-rm -rf cdm
-echo "DONE"
-
 #install i3-gaps through AUR
 echo "installing i3-gaps"
 echo "cloning"
-git clone https://aur.archlinux.org/i3-gaps-git.git
-cd i3-gaps-git
+git clone https://aur.archlinux.org/i3-gaps-git.git ie-gaps
+cd i3-gaps
 echo "instaling"
 makepkg -si
 cd ..
-rm -rf i3-gaps-git
 echo "done"
 
 echo ""
