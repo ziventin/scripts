7906d72238394d73efaf2110229347672214497c
ziventin <ziventin@gmail.com>
2017-08-02 23:12:26 +0300

    fixed urxvt config problem and added another bind

--- i3_auto.sh
+++ i3_auto.sh
@@ -5,6 +5,7 @@ i3-msg "workspace 1; append_layout ~/scripts/i3_layouts/work1.json"
 i3-msg "workspace 2; append_layout ~/scripts/i3_layouts/work2.json"
 i3-msg "workspace 4; append_layout ~/scripts/i3_layouts/work4.json"
 
+xrdb ~/.Xresources
 (urxvt &)
 (urxvt &)
 (firefox &)
