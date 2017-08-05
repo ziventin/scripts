8f04f0d763a90985e3ffad212f29640c617c4369
ziventin <ziventin@gmail.com>
2017-08-05 20:28:02 +0300

    changed script usage from use as user to use as root

--- setup.sh
+++ setup.sh
@@ -1,66 +1,83 @@
 #!/bin/bash
 #an easy method to install my configs and scripts
 #for archlinux
+set $ins pacman -Sq
+
 echo "starting script"
+echo ""
+
+echo "creating user"
+useradd -m $1
+
+echo "seting password"
+passwd $1
 
 # install git
 echo "installing git"
-sudo pacman -S git
+$ins git
 echo "DONE"
 
 #clone scripts and configs
 echo "cloning scripts and configs"
+cd /home/$1
 git clone https://www.github.com/ziventin/scripts
-echo "DONE"
 cd scripts
 
 #put bashrc into the correct location
 echo "moving .bashrc"
-ln -sf ~/scripts/bashrc ~/.bashrc
-echo "DONE"
+ln -sf /home/$1/scripts/bashrc /home/$1/.bashrc 
 
 #put xinitrc into the correct location
 echo "moving .xinitrc"
-ln -sf ~/scritps/xinitrc ~/.xinitrc
-echo "DONE"
+ln -sf /home/$1/scritps/xinitrc /home/$1/.xinitrc
 
 #put Xresources into the correct location
 echo "moving .Xresources"
-ln -sf ~/scripts/Xresources ~/.Xresources
-echo "DONE"
+ln -sf /home/$1/scripts/Xresources /home/$1/.Xresources
 
 #put i3 config into the correct location
-mkdir ~/.config
-mkdir ~/.config/i3
+mkdir /home/$1/.config
+mkdir /home/$1/.config/i3
 echo "moving i3 config"
-ln -sf ~/scritps/i3_config ~/.config/i3/config
-echo "DONE"
+ln -sf /home/$1/scritps/i3_config /home/$1/.config/i3/config
 
 #put vimrc into the correct location
 echo "moving .vimrc"
-ln -sf ~/scripts/vimrc ~/.vimrc
-echo "DONE"
+ln -sf /home/$1/scripts/vimrc /home/$1/.vimrc
 
 #clone my firefox start page
 echo "cloning firefox startpage"
 git clone https://www.github.com/ziventin/startpage startpage
-echo "DONE"
+
+#give ownership of teh files to the user
+chown -f -R $1:$1 /home/$1/scripts
 
 #install programs
 echo "installing programs"
-sudo pacman -S firefox rxvt-unicode feh mesa xorg screenfetch base-devel vim i3blocks i3status i3lock
+echo ""
+$ins firefox
+$ins sudo
+$ins rxvt-unicode
+$ins feh
+$ins mesa
+$ins xorg
+$ins screenfetch
+$ins base-devel
+$ins vim
+$ins i3blocks
+$ins i3status
+$ins i3lock
+$ins pulseaudio
 echo "DONE"
 
 #install i3-gaps through AUR
 echo "installing i3-gaps"
-echo "cloning"
 git clone https://aur.archlinux.org/i3-gaps-git.git i3-gaps
+chown -f -R $1:$1 /home/$1/scripts
 cd i3-gaps
-echo "instaling"
-makepkg -si
+su $1 -c "makepkg -si"
 cd ..
-echo "done"
 
 echo ""
-echo "all programs and scripts installed"
+echo "All programs and scripts installed."
 exit
